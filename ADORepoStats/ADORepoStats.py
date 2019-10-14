a49e6b27adff5f78a581c9bdb86f7cfe5731bce7
domroutley <dominic.routley@gmail.com>
2019-10-14 12:37:53 +0100

    added init getproject

--- ADORepoStats/ADORepoStats.py
+++ ADORepoStats/ADORepoStats.py
@@ -1,7 +1,36 @@
-
+import requests
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
 
 class ADORepoStats:
 
-    def __init__(self, project, repo="_all"):
-        self.project = project
-        self.repo = repo
+    def __init__(self, organisation, project, repository="", personalAccessToken):
+        self.org = organisation
+        self.proj = project
+        self.repo = repository
+        self.pat = personalAccessToken
+
+    def getProject(self):
+
+        organization_url = 'https://dev.azure.com/' + self.org
+
+        # Create a connection to the org
+        credentials = BasicAuthentication('', self.pat)
+        connection = Connection(base_url=organization_url, creds=credentials)
+
+        # Get a client (the "core" client provides access to projects, teams, etc)
+        core_client = connection.clients.get_core_client()
+
+        # Get the first page of projects
+        get_projects_response = core_client.get_projects()
+        index = 0
+        while get_projects_response is not None:
+            for project in get_projects_response.value:
+                pprint.pprint("[" + str(index) + "] " + project.name)
+                index += 1
+            if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
+                # Get the next page of projects
+                get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
+            else:
+                # All projects have been retrieved
+                get_projects_response = None
