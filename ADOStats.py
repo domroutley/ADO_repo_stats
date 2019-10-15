889f8a141c4a02f4d13b206d303ae9d704ab0eae
domroutley <dominic.routley@gmail.com>
2019-10-15 11:57:03 +0100

    connection class variable

--- ADOStats.py
+++ ADOStats.py
@@ -4,26 +4,27 @@ from msrest.authentication import BasicAuthentication
 
 class Project:
 
+    # initilises connection with ADO
     def __init__(self, organisation, projectName, personalAccessToken):
         self.org = organisation
-        self.pn = projectName
+        pn = projectName
         self.pat = personalAccessToken
 
         organizationUrl = 'https://dev.azure.com/' + self.org
 
         # Create a connection to the org
         credentials = BasicAuthentication('', self.pat)
-        connection = Connection(base_url=organizationUrl, creds=credentials)
+        self.connection = Connection(base_url=organizationUrl, creds=credentials)
 
         # Get a client (the "core" client provides access to projects, teams, etc)
-        coreClient = connection.clients.get_core_client()
+        coreClient = self.connection.clients.get_core_client()
 
         # Get the first page of projects
         getProjectsResponse = coreClient.get_projects()
         index = 0
         while getProjectsResponse is not None:
             for project in getProjectsResponse.value:
-                if (project.name == self.pn):
+                if (project.name == pn):
                     self.project = project
                     # Stop grabbing stuff
                     getProjectsResponse.continuation_token = None
