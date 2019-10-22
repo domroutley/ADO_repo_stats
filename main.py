8255d53f61525acbacfde1ca4dce324a767eea39
domroutley <dominic.routley@gmail.com>
2019-10-22 10:47:02 +0100

    added docstring

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import csv
 from AzureDevOpsWrapper import Project
 
 def main(organisationName, projectName, pat):
+    """Creates five csv files containing raw statistics about the given project.
+
+    :param organisationName: The name of the organisation that contains the target project.
+    :organisationName type: <String>
+
+    :param projectName: The name of the target project.
+    :projectName type: <String>
+
+    :param pat: A valid personal access token to be used for access to the project.
+    :pat type: <String>
+
+    :return: None
+    """
     pass
 
 
