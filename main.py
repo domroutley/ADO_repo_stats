dd2428b9bac3dfe88483e9040eb1f7272507a082
domroutley <dominic.routley@gmail.com>
2019-10-22 10:27:30 +0100

    new main file and reorg of project class

---
+++ main.py
@@ -0,0 +1,10 @@
+import csv
+from AzureDevOpsWrapper import Project
+
+def main(organisationName, projectName, pat):
+    pass
+
+
+if __name__ == "__main__":
+    import target
+    main(target.organisation, target.project, target.pat)
