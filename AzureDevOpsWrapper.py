7b82ebe3ca63e2d1f4fcaddad0aab81e85351c0b
domroutley <dominic.routley@gmail.com>
2019-10-23 11:11:08 +0100

    added exception for bad project name

--- AzureDevOpsWrapper.py
+++ AzureDevOpsWrapper.py
@@ -33,21 +33,20 @@ class Project:
 
         # Get the first page of projects
         getProjectsResponse = coreClient.get_projects()
-        index = 0
         while getProjectsResponse is not None:
             for project in getProjectsResponse.value:
                 if (project.name == pn):
                     self.project = project
                     # Stop grabbing stuff
                     getProjectsResponse.continuation_token = None
-                    break
-                index += 1
+                    return
             if getProjectsResponse.continuation_token is not None and getProjectsResponse.continuation_token != "":
                 # Get the next page of projects
                 getProjectsResponse = coreClient.get_projects(continuation_token=getProjectsResponse.continuation_token)
             else:
                 # All projects have been retrieved
                 getProjectsResponse = None
+                raise Exception('Project "' + pn + '" was not found.')
 
 
     def getRepositories(self):
