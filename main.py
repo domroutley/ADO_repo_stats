f1085ffa12681f4b3c23162b6295adf21086268d
domroutley <dominic.routley@gmail.com>
2019-10-23 12:03:43 +0100

    added check for if lists are bigger than 0

--- main.py
+++ main.py
@@ -75,22 +75,27 @@ def createBuildStructure(builds, listOfDefinitions):
     :rtype: <List> of type <String>
     """
     myList = []
-    # create list of keys
-    keys = ['name', 'succeeded', 'partiallySucceeded', 'cancelled', 'failed', 'none', 'total']
-    for definition in listOfDefinitions:
-        # Add all definition names to the list
-        #   This will mean that even if they have no builds associated they are still represented
-        #   We also set all of the possible results to 0
-        myList.append({'name': definition.name, 'succeeded': 0, 'partiallySucceeded': 0, 'cancelled': 0, 'failed': 0, 'none': 0, 'total': 0})
-    for item in builds:
-        for definitionDict in myList:
-            # If this is the same definition (in list to be filled and list to take things from)
-            if item.definition.name == definitionDict['name']:
-                definitionDict[item.result] += 1
-                # We always want to increment the total
-                definitionDict['total'] += 1
-                # As we have found the definition in both, we wont again, so we can break to avoid pointless looping
-                break
+    keys = []
+    if len(listOfDefinitions) > 0:
+        for definition in listOfDefinitions:
+            # Add all definition names to the list
+            #   This will mean that even if they have no builds associated they are still represented
+            #   We also set all of the possible results to 0
+            myList.append({'name': definition.name, 'succeeded': 0, 'partiallySucceeded': 0, 'cancelled': 0, 'failed': 0, 'none': 0, 'total': 0})
+        # create list of keys, pulls keys from above dictionary
+        for key in myList[0]:
+            keys.append(key)
+
+    if len(builds) > 0:
+        for item in builds:
+            for definitionDict in myList:
+                # If this is the same definition (in list to be filled and list to take things from)
+                if item.definition.name == definitionDict['name']:
+                    definitionDict[item.result] += 1
+                    # We always want to increment the total
+                    definitionDict['total'] += 1
+                    # As we have found the definition in both, we wont again, so we can break to avoid pointless looping
+                    break
     return myList, keys
 
 
@@ -110,20 +115,27 @@ def createReleaseStructure(releases, listOfDefinitions):
     :rtype: <List> of type <String>
     """
     myList = []
-    keys = ['name', 'total']
-    for definition in listOfDefinitions:
-        myList.append({'name': definition.name, 'total': 0})
-    for item in releases:
-        for definitionDict in myList:
-            if item.release_definition.name == definitionDict['name']:
-                if not item.status in definitionDict:
-                    definitionDict[item.status] = 1
-                    if not item.status in keys:
-                        keys.append(item.status)
-                else:
-                    definitionDict[item.status] += 1
-                definitionDict['total'] += 1
-                break
+    keys = []
+    if len(listOfDefinitions) > 0:
+        for definition in listOfDefinitions:
+            myList.append({'name': definition.name, 'total': 0})
+        # create list of keys, pulls keys from above dictionary
+        for key in myList[0]:
+            keys.append(key)
+
+    if len(releases) > 0:
+        for item in releases:
+            for definitionDict in myList:
+                if item.release_definition.name == definitionDict['name']:
+                    if not item.status in definitionDict:
+                        definitionDict[item.status] = 1
+                        if not item.status in keys:
+                            keys.append(item.status)
+                    else:
+                        definitionDict[item.status] += 1
+                    definitionDict['total'] += 1
+                    break
+    return myList, keys
     return myList, keys
 
 
